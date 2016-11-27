cba6bd0e9508195fd9a38af4661cb46c8cb7dee9
Nikos <niopas@inf.uth.gr>
2016-11-27 13:36:12 +0200

    Login from local Database

---
+++ src/java/com/example/web/DbLogin.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.web;
+
+import com.example.model.LoginListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author nick
+ */
+public class DbLogin extends HttpServlet {
+    Connection conn=null;
+    PreparedStatement pst=null;
+    ResultSet rs=null;
+    
+    @Override
+     public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+         
+        int SessionNum;
+        
+        PrintWriter out=response.getWriter();
+        response.setContentType("text/html");
+        LoginListener loginSession=new LoginListener();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        conn=MySqlConnect.ConnectDB();
+        if (conn!= null) {
+            out.println("connection established");
+        }
+        String sql = "SELECT * FROM credentials WHERE username=? AND password=?";
+        try {
+            pst=conn.prepareStatement(sql);
+            pst.setString(1, username);
+            pst.setString(2, password);
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                out.println("Good");
+            }
+            else {
+                out.println("Wrong username and password. Try again.");
+            }
+        } catch (Exception e) {
+            out.println(e);
+        }
+         
+     }
+
+}
---
+++ src/java/com/example/web/MySqlConnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.web;
+
+import java.io.PrintWriter;
+import java.sql.*;
+
+/**
+ *
+ * @author nick
+ */
+public class MySqlConnect {
+    Connection conn=null;
+    
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb", "root", "12312312333");
+            return conn;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
